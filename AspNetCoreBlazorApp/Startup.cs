445ca5bd0d55b2e7439b32fe8445b9755ddeb603
Man Ioan <man_ioan@live.com>
2021-10-24 16:10:21 +0300

    Implement test blazor component

--- AspNetCoreBlazorApp/Startup.cs
+++ AspNetCoreBlazorApp/Startup.cs
@@ -21,12 +21,14 @@ namespace AspNetCoreBlazorApp
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddServerSideBlazor();
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.  
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +38,7 @@ namespace AspNetCoreBlazorApp
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.  
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
@@ -51,7 +53,10 @@ namespace AspNetCoreBlazorApp
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapBlazorHub();
             });
+
         }
+
     }
 }
